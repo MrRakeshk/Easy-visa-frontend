6d4188a6c3491d3fee0b4fbb6108121e28b9c832
Rakesh <112494957+MrRakeshk@users.noreply.github.com>
2025-03-14 00:50:14 +0530

    Update Errorpage.jsx

--- src/Components/Shared/Errorpage/Errorpage.jsx
+++ src/Components/Shared/Errorpage/Errorpage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Helmet>
-        <title>VisaEase | Not-Found</title>
+        <title>EasyVisa | Not-Found</title>
       </Helmet>
       <div className="text-center">
         <h1 className="text-6xl font-bold text-red-500">404</h1>
